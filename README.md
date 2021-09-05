2876c7b8a4529320632926f492d9033dd04bd079
iVuDang <vdang159@mtroyal.ca>
2021-09-05 12:24:32 -0600

    uploading project

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # The-Odin-Project-Etch-a-Sketch
-I created an Etch a Sketch (user is able to draw on a sketchpad) | Skills: JavaScript, DOM, HTML, CSS. 
+## Instructions
+https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project
+
+## Purpose: 
+To build a browser version of something between a sketchpad and an Etch-A-Sketch.
+
+## Preview:
+<img src="https://raw.githubusercontent.com/iVuDang/The-Odin-Project-Etch-a-Sketch/07f76818592eb03c74305eb8d1b550363e54ada2/Etch-a-Sketch-iVuDang.png" width=75% height=75%>
+
+### Technologies: 
+* JavaScript
+* HTML
+* CSS
+
+
+### Tools
+* Git
+* GitHub
+
+
+### Outcome :white_check_mark:
+I created an Etch a Sketch (user is able to draw on a sketchpad) | Skills: JavaScript, DOM, HTML, CSS.
+
+
+
+| Website | Link | 
+| ------------- | ------------- | 
+| CodePen demo | https://codepen.io/iVuDang/full/522d0a34ca84c28e811f17482156781e | 
+| GitHub demo | https://ivudang.github.io/The-Odin-Project-Rock-Paper-Scissors/ | 
+
+
+<br />
+
+## Summary
+
+### What I learned
+1. querySelector vs. getElementById
+* They both select elements from a web page. They do so in different ways.
+* querySelector method lets you retrieve an element using a CSS selector query, you can select elements by ID, class, or any other type of selector. 
+* getElementById method retrieves an element by its DOM ID.
+
+You should opt to use the querySelector method if you need to select elements using more complex rules that are easily represented using a CSS selector. If you want to select an element by its ID, using getElementById is a good choice.
+
+
+### What could be improved
+- [ ] How to change 'leaf' to 'earth' on output. 
+
+
+
---
+++ script.js
@@ -0,0 +1,85 @@
+// Set up variables to retrieve HTML and CSS elements
+const grid = document.querySelector('.grid');
+const reset = document.querySelector('.reset');
+
+const defaultColor = document.querySelector('.default');
+const randomColor = document.querySelector('.color'); 
+
+const resolutionInput = document.querySelector('#resolutionInput'); 
+const resolutionLabel = document.querySelector('.resolutionLabel')
+let numSquares = 8;  
+
+
+// Updates the text above our input range 
+resolutionInput.addEventListener('input', changeGrid); 
+
+function changeGrid(e){
+  numSquares = e.target.value; 
+  resolutionLabel.textContent = `${numSquares}x${numSquares}`;
+}
+
+
+// Creates our grid div boxes on load
+createGrid(numSquares);
+
+
+// Function to create the grid div boxes, retrieve CSS, add 'mouseover' effect, and append to HTML
+function createGrid(resolutionInput){
+  let gridSize = (resolutionInput * resolutionInput); 
+    
+  for (i = 0; i < gridSize; ++i){ 
+      let gridDiv = document.createElement('div') 
+      gridDiv.className = 'box' 
+      gridDiv.addEventListener('mouseover', changeColor); 
+    
+      grid.appendChild(gridDiv)
+  }
+
+  grid.style.gridTemplateColumns =`repeat(${resolutionInput}, 1fr)` 
+  grid.style.gridTemplateRows =`repeat(${resolutionInput}, 1fr)`
+}
+
+
+// Added event listener for our reset button, (type of event, function to execute)
+reset.addEventListener('click', resetGrid);
+
+// Function to clear the grid divs, and call on createGrid to create a new set of grid divs
+function resetGrid() {
+  while (grid.firstChild) { 
+    grid.removeChild(grid.firstChild);  
+  }
+  
+  createGrid(numSquares); 
+}
+
+
+// Function for 'mouseover' event, changes background color based on which type is 'active'
+function changeColor(e) { 
+  if (type === "randomColorMode") {  
+    const randomR = Math.floor(Math.random() * 256) 
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (type === "defaultColorMode") {
+    e.target.style.backgroundColor = 'lightpink' ;
+  }
+}; 
+
+
+// Default button - makes the defaultColorMode type active, and removes active from randomColorMode
+defaultColor.addEventListener("click", function() {
+  type = "defaultColorMode";
+  
+  this.classList.add("active");
+  randomColor.classList.remove("active");
+});
+
+// Color button - makes the randomColorMode type active, and removes active from defaultColorMode
+randomColor.addEventListener("click", function() {
+  type = "randomColorMode";
+  
+  this.classList.add("active");
+  defaultColor.classList.remove("active");
+});
+
+
